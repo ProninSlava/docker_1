4057e81ee9a6b36c34f68f767fffa1d01eff4594
Вячеслав Валерьевич Пронин <99884823+ProninSlava@users.noreply.github.com>
2022-12-28 18:19:01 +0300

    Update settings.py

--- weapon_api/weapon/settings.py
+++ weapon_api/weapon/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'weapon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'weapon',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': 'slava',
-        'PASSWORD': '7548',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
